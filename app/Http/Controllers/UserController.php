bf35e24d5147d6e556bd07dccee185b0b1ce1eba
Ronaldo Pires Dias <ronaldo@seujoao.com>
2023-09-05 22:20:09 -0400

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,26 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Listar todos os usuários (para administradores)
         $users = User::all();
-        return view('users.index', compact('users'));
+        return $users;
+        // return view('users.index', compact('users'));
     }
 
     public function create()
     {
-        // Exibir formulário de criação de usuário (para administradores)
         return view('users.create');
     }
 
     public function store(Request $request)
     {
-        // Salvar um novo usuário no banco de dados (para administradores)
-        // Valide os dados recebidos antes de salvar
         User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
@@ -37,14 +35,11 @@ class UserController extends Controller
 
     public function edit(User $user)
     {
-        // Exibir formulário de edição de usuário (para administradores)
         return view('users.edit', compact('user'));
     }
 
     public function update(Request $request, User $user)
     {
-        // Atualizar informações do usuário (para administradores)
-        // Valide os dados recebidos antes de atualizar
         $user->update([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
@@ -56,8 +51,23 @@ class UserController extends Controller
 
     public function destroy(User $user)
     {
-        // Excluir um usuário (para administradores)
         $user->delete();
-        return redirect()->route('users.index')->with('success', 'Usuário excluído com sucesso.');
+        // return redirect()->route('users.index')->with('success', 'Usuário excluído com sucesso.');
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($loginData)) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+
+        // $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => Auth::user()]);
     }
 }
